1fed000db419ef3fe20a37a21c9e3d04e5dc52a9
Shi Lei <shi_lei@massclouds.com>
2024-03-19 21:53:08 +0800

    static app - init_static can work

--- scripts/make/utils.mk
+++ scripts/make/utils.mk
@@ -28,7 +28,7 @@ define build_linux_image
   @sudo mkdir -p ./mnt/lib
   @sudo mkdir -p ./mnt/sbin
   @sudo cp ./payload/ld-linux-riscv64-lp64d.so.1 ./mnt/lib/
-  @sudo cp ./payload/init ./mnt/sbin/
+  @sudo cp ./payload/init_static ./mnt/sbin/init
   ls -l ./mnt/lib
   @sudo umount ./mnt
   @rm -rf mnt
